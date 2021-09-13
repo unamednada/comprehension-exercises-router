830056323c064c47c36e56e148be14180e202e2d
unamednada <dias.gustavo.dias@gmail.com>
2021-09-13 10:34:02 -0300

    Finish exercises 1 to 6 - add route and link to components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <ul>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/users">Users</Link></li>
+          <li><Link to="/">Back to Homepage</Link></li>
+        </ul>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about" component={About} />
+        <Route path="/users" component={Users} />
+      </BrowserRouter>
     );
   }
 }
